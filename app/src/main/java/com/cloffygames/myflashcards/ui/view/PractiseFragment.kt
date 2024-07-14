70c2c3e9cd35fc1baca1d2665ebe9aa1b1a6b9e7
serhattastan <serhattastannn@gmail.com>
2024-07-15 01:54:48 +0300

    feat: Add new application logo
    - Integrated a modern and vibrant logo into the app
    - Updated relevant drawable resources with the new logo design
    - Ensured the logo is displayed on the splash screen and main activities

--- app/src/main/java/com/cloffygames/myflashcards/ui/view/PractiseFragment.kt
+++ app/src/main/java/com/cloffygames/myflashcards/ui/view/PractiseFragment.kt
@@ -21,12 +21,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.Locale
 
-@AndroidEntryPoint
+@AndroidEntryPoint // Bu anotasyon, Dagger Hilt'in bu sınıfa bağımlılık enjeksiyonu yapacağını belirtir
 class PractiseFragment : Fragment(), TextToSpeech.OnInitListener, TextToSpeech.OnUtteranceCompletedListener {
 
     private var _binding: FragmentPractiseBinding? = null
     private val binding get() = _binding!!
 
+    // PractiseViewModel'i, Hilt'in viewModels() yardımcı fonksiyonu ile başlatır
     private val viewModel: PractiseViewModel by viewModels()
     private val args: PractiseFragmentArgs by navArgs()
     private lateinit var tts: TextToSpeech
@@ -35,12 +36,14 @@ class PractiseFragment : Fragment(), TextToSpeech.OnInitListener, TextToSpeech.O
     private var originalColor: Int = 0
     private var currentlySpeakingTextView: TextView? = null
 
+    // Kartların sırasını tutan liste
     private val cardSequence = mutableListOf<Int>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // FragmentPractiseBinding'i inflate eder ve _binding değişkenine atar
         _binding = FragmentPractiseBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -48,27 +51,34 @@ class PractiseFragment : Fragment(), TextToSpeech.OnInitListener, TextToSpeech.O
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // TextToSpeech (TTS) motorunu başlatır
         tts = TextToSpeech(context, this)
 
+        // Grup verilerini yükler ve gösterir
         loadGroupData()
 
+        // Ses ikonuna tıklama olayını dinler ve metni seslendirir
         binding.soundImage.setOnClickListener {
             speakText(binding.practiseTermText, Locale.getDefault())
         }
 
+        // Sol ok ikonuna tıklama olayını dinler ve önceki kartı gösterir
         binding.leftButtonImage.setOnClickListener {
             showPreviousCard()
         }
 
+        // Sağ ok ikonuna tıklama olayını dinler ve sonraki kartı gösterir
         binding.rightButtonImage.setOnClickListener {
             showNextCard()
         }
 
+        // Geri butonuna tıklama olayını dinler ve önceki ekrana döner
         binding.backGroupDetailButton.setOnClickListener {
             findNavController().navigateUp()
         }
     }
 
+    // Grup verilerini ve kartları yükler
     private fun loadGroupData() {
         CoroutineScope(Dispatchers.Main).launch {
             val cardGroup = viewModel.loadCardGroup(args.groupId)
@@ -84,16 +94,19 @@ class PractiseFragment : Fragment(), TextToSpeech.OnInitListener, TextToSpeech.O
         }
     }
 
+    // Sonraki kartı gösterir
     private fun showNextCard() {
         currentIndex = (currentIndex + 1) % cardSequence.size
         displayCard()
     }
 
+    // Önceki kartı gösterir
     private fun showPreviousCard() {
         currentIndex = if (currentIndex - 1 < 0) cardSequence.size - 1 else currentIndex - 1
         displayCard()
     }
 
+    // Şu anki kartı ekranda gösterir
     private fun displayCard() {
         val cardIndex = cardSequence[currentIndex]
         binding.practiseTermText.text = cards[cardIndex].term
@@ -101,6 +114,7 @@ class PractiseFragment : Fragment(), TextToSpeech.OnInitListener, TextToSpeech.O
         binding.practiseTermText.setTextColor(resources.getColor(R.color.secondary_color))
     }
 
+    // Belirtilen TextView'in metnini seslendirir ve metin rengini değiştirir
     private fun speakText(textView: TextView, locale: Locale) {
         stopSpeakingIfNeeded()
         tts.language = locale
@@ -113,6 +127,7 @@ class PractiseFragment : Fragment(), TextToSpeech.OnInitListener, TextToSpeech.O
         tts.speak(textToRead, TextToSpeech.QUEUE_FLUSH, params, textToRead)
     }
 
+    // TTS konuşuyorsa durdurur ve metin rengini eski haline getirir
     private fun stopSpeakingIfNeeded() {
         if (tts.isSpeaking) {
             tts.stop()
@@ -123,6 +138,7 @@ class PractiseFragment : Fragment(), TextToSpeech.OnInitListener, TextToSpeech.O
         }
     }
 
+    // TTS motoru başlatıldığında çağrılır
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
             tts.language = Locale.getDefault()
@@ -130,6 +146,7 @@ class PractiseFragment : Fragment(), TextToSpeech.OnInitListener, TextToSpeech.O
         }
     }
 
+    // TTS konuşması tamamlandığında çağrılır
     override fun onUtteranceCompleted(utteranceId: String?) {
         activity?.runOnUiThread {
             currentlySpeakingTextView?.let {
@@ -139,6 +156,7 @@ class PractiseFragment : Fragment(), TextToSpeech.OnInitListener, TextToSpeech.O
         }
     }
 
+    // Görünüm yok edilirken çağrılır, TTS motorunu kapatır
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
